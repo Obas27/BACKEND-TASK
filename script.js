0dddd29dd1861d3b2dce3d98c574dfbaf7d4a7d2
Obas27 <obasogieosayande0027@gmail.com>
2024-12-12 00:44:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let email = "YourEmail";
+console.log(email)
+
+var confirm_Email = "YourEmailConfirm";
+console.log(confirm_Email)
+
+let password = "Password";
+console.log(password)
+
+const WhatShouldWeCallYou = "UserName";
+console.log(WhatShouldWeCallYou)
+
+let date_Of_Birth_Month = "Your Month Of Birth";
+console.log(date_Of_Birth_Month)
+
+let date_Of_Birth_Day = "Your Day Of Birth";
+console.log(date_Of_Birth_Day)
+
+let date_Of_Birth_Year= "Your Year Of Birth";
+console.log(date_Of_Birth_Year)
+
+let gender = "Male";
+console.log(gender)
+
+let gender_Of_Women = "Female";
+console.log(gender_Of_Women)
---
+++ script2.js
@@ -0,0 +1,30 @@
+ const Name = "First Name";
+ console.log(Name)
+ const name2 = "Last Name";
+ console.log(name2)
+ var Street_Address  = "Address";
+ console.log(Street_Address)
+ let Street_Address_Line = "Address2";
+ console.log(Street_Address_Line)
+ let  Input_Your_City = "City"
+ console.log(Input_Your_City)
+ let  Input_Your_State = "State"
+ console.log(Input_Your_State)
+ let  Input_Your_Code = "PostalCode"
+ console.log(Input_Your_Code)
+ let  Select_Your_Country = "Country"
+ console.log(Select_Your_Country)
+ let email = "E-mail";
+ console.log(email)
+ let Home_Number = "Home Number";
+ console.log(Home_Number)
+ let Cellular_Number = "Cellular Number";
+ console.log(Cellular_Number)
+ let Work_Number = "Work Number";
+ console.log(Work_Number)
+ let Your_Website = "Website"
+ console.log(Your_Website)
+ let Your_Term = "Membership Term"
+ console.log(Your_Term)
+ const Your_Signature = "Signature"
+ console.log(Your_Signature)
