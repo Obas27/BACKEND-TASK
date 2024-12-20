2e81430820ff532f623fb47b8237e9ffd47387fb
Obas27 <obasogieosayande0027@gmail.com>
2024-12-20 02:18:07 +0100

    Add files via upload

---
+++ scriptdatatypes.js
@@ -0,0 +1,25 @@
+let password = 9099880
+console.log(typeof(password));
+
+let Username = "potato"
+console.log(typeof(Username));
+let telephone = "09012345678"
+console.log(typeof(telephone));
+let account_Balance = 6n
+console.log(typeof(account_Balance));
+let place_Of_Work = null
+console.log(typeof(place_Of_Work));
+let street = "NO MAN'S LAND"
+console.log(typeof(street));
+let date_Of_Birth = null;
+console.log(typeof(date_Of_Birth));
+let allergies = false
+console.log(typeof(allergies));
+let alive = true
+console.log(typeof(alive));
+
+// let spy_Name = x 
+// console.log(typeof(spy_Name));
+ likes = ["oranges","13","potato"]
+console.log(typeof(likes));
+
